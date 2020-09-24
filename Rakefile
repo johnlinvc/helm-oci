2c22b15c68b03fc587a6efeb2c9a3d03d5f88be2
John Lin <john.lin@westpharma.com>
2020-09-24 15:10:13 +0800

    able to get tags as versions

--- Rakefile
+++ Rakefile
@@ -3,7 +3,7 @@ task :build do
   Dir.mkdir("tmp") unless File.exists?("tmp")
   system("../mruby/mruby-mac/bin/mrbc -o tmp/helm_oci.c -Bhelm_oci src/helm_oci.rb")
   Dir.mkdir("bin") unless File.exists?("bin")
-  system("gcc -std=c99 -I../mruby/mruby-mac/include src/main.c -o bin/helm-oci ../mruby/mruby-mac/build/host/lib/libmruby.a ../mruby/mruby-mac/build/host/mrbgems/mruby-yaml/yaml-0.2.2/build/lib/libyaml.a -lm")
+  system("gcc -std=c99 -I../mruby/mruby-mac/include src/main.c -o bin/helm-oci ../mruby/mruby-mac/build/host/lib/libmruby.a ../mruby/mruby-mac/build/host/mrbgems/mruby-yaml/yaml-0.2.2/build/lib/libyaml.a -lm -lcurl")
 end
 
 task :default => [:build]
--- src/helm_oci.rb
+++ src/helm_oci.rb
@@ -7,13 +7,39 @@ class HelmOci
       $stderr.puts `#{full_cmd}`
     end
 
+    def auth_param
+      "#{@user}:#{@pw}"
+    end
+
+    def log(*obj)
+      $stderr.puts(*obj)
+    end
+
+    def query_oci(*paths)
+      uri = ["https://#{auth_param}@#{@registry}",*paths].join("/")
+      $stderr.puts(uri)
+      curl = Curl.new
+      response = curl.get(uri)
+      json_body = JSON.parse(response.body)
+      log(json_body)
+      json_body
+    end
+
+    def get_chart_versions(chart)
+      tag_list = query_oci("v2/#{chart}/tags/list")
+      tag_list["tags"].map do |tag|
+        {
+          "apiVersion" => "v2",
+          "version" => tag,
+          "name" => chart,
+          "urls" => ["oci+login://#{auth_param}@#{@repo}/menifests/#{tag}"]
+        }
+      end
+    end
+
     def gen_index(chart)
       $stderr.puts("gen index for #{chart}")
-      versions = [{
-        "apiVersion" => "v1",
-        "version" => "7.0.1",
-        "name" => chart
-      }]
+      versions = get_chart_versions(chart)
       yaml = {"apiVersion" => "v1", "entries" => {chart => versions}}
       yaml_s = YAML.dump(yaml)
       $stderr.puts(yaml_s)
@@ -22,8 +48,8 @@ class HelmOci
 
     def parse_arg(argv)
       $stderr.puts(argv)
-      uri = argv[4]
-      uri =~ /^oci\+login:\/\/([^:]+):([^@]+)@(.*)\/([^\/]+)$/
+      @uri = argv[4]
+      @uri =~ /^oci\+login:\/\/([^:]+):([^@]+)@(.*)\/([^\/]+)$/
       if !$~
           $stderr.puts("repo format error")
       end
@@ -37,7 +63,6 @@ class HelmOci
 
     def run(argv)
       parse_arg(argv)
-      helm_exec("registry login -u #{@user} -p #{@pw} #{@registry}")
       case @action
       when "index.yaml"
         gen_index(@chart)
