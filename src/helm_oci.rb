47c684934182ef21826bb60d254c758ed592c725
johnlinvc <johnlinvc@gmail.com>
2020-09-25 15:22:08 +0800

    get user/pw via env OCI_USER OCI_PW, add log toggle HELM_OCI_DEBUG

--- src/helm_oci.rb
+++ src/helm_oci.rb
@@ -7,17 +7,24 @@ class HelmOci
       log `#{full_cmd}`
     end
 
+    def user
+      ENV['OCI_USER']
+    end
+
+    def pw
+      ENV['OCI_PW']
+    end
+
     def auth_param
-      "#{@user}:#{@pw}"
+      "#{user}:#{pw}"
     end
 
     def log(*obj)
-      $stderr.puts(*obj)
+      $stderr.puts(*obj) if ENV["HELM_OCI_DEBUG"] == "1"
     end
 
     def query_oci(*paths)
       uri = ["https://#{auth_param}@#{@registry}",*paths].join("/")
-      log(uri)
       curl = Curl.new
       response = curl.get(uri)
       json_body = JSON.parse(response.body)
@@ -32,7 +39,7 @@ class HelmOci
           "apiVersion" => "v2",
           "version" => tag,
           "name" => chart,
-          "urls" => ["oci+login://#{auth_param}@#{@repo}/#{chart}-#{tag}.tgz?tag=#{tag}"]
+          "urls" => ["oci+login://#{@repo}/#{chart}-#{tag}.tgz?tag=#{tag}"]
         }
       end
     end
@@ -49,11 +56,11 @@ class HelmOci
     def parse_arg(argv)
       log(argv)
       @uri = argv[4]
-      @uri =~ /^oci\+login:\/\/([^:]+):([^@]+)@(.*)\/([^\/]+)$/
+      @uri =~ /^oci\+login:\/\/(.*)\/([^\/]+)$/
       if !$~
           log("repo format error")
       end
-      @user, @pw, @repo, @action = $~.captures
+      @repo, @action = $~.captures
       @repo =~ /^([^\/]+)\/(.+)$/
       if !$~
           log("registry format error")
@@ -68,7 +75,7 @@ class HelmOci
       }
       log(@chart, version)
       log(dir)
-      helm_exec("registry login -u #{@user} -p #{@pw} #{@registry}")
+      helm_exec("registry login -u #{user} -p #{pw} #{@registry}")
       helm_exec("chart pull #{@registry}/#{@chart}:#{version}")
       helm_exec("chart export #{@registry}/#{@chart}:#{version} -d #{dir}")
       helm_exec("package #{dir}/#{@chart} -d #{dir} --version #{version}")
